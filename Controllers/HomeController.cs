b0f0b75ecdffda960848655292fe5a3335e0f313
roninout <roninorgua@gmail.com>
2019-07-17 12:01:12 +0300

    ууу

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BookingAppStore.Models;
+using BookingAppStore.Util;
 
 namespace BookingAppStore.Controllers
 {
@@ -21,5 +23,68 @@ namespace BookingAppStore.Controllers
             // возвращаем представление
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир!</h2>");
+        }
+
+        public ActionResult GetImage()
+        {
+            string path = "../Images/visualstudio.png";
+            return new ImageResult(path);
+        }
+
+        #region Files
+        public FileResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Server.MapPath("~/Files/PDFIcon.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательно
+            string file_name = "PDFIcon.pdf";
+            return File(file_path, file_type, file_name);
+        }
+
+        // Отправка массива байтов
+        public FileResult GetBytes()
+        {
+            string path = Server.MapPath("~/Files/PDFIcon.pdf");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "application/pdf";
+            string file_name = "PDFIcon.pdf";
+            return File(mas, file_type, file_name);
+        }
+
+        // Отправка потока
+        public FileResult GetStream()
+        {
+            string path = Server.MapPath("~/Files/PDFIcon.pdf");
+            // Объект Stream
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "application/pdf";
+            string file_name = "PDFIcon.pdf";
+            return File(fs, file_type, file_name);
+        }
+        #endregion
     }
 }
